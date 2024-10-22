60a7d493d9f737e90b842512e4ee3f25060f364d
MASSHUU12 <gawrysiak.maciej@proton.me>
2024-10-22 13:30:41 +0200

    feat(lab3): restore working state & add support for resolution argument

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -5,8 +5,8 @@
 #include <chrono>
 
 // Screen coordinate
-int IX_MAX{};
-int IY_MAX{};
+int ix_max{};
+int iy_max{};
 
 // World coordinate = parameter plane
 constexpr double CX_MIN = -2.5;
@@ -14,8 +14,8 @@ constexpr double CX_MAX = 1.5;
 constexpr double CY_MIN = -2.0;
 constexpr double CY_MAX = 2.0;
 
-double PIXEL_WIDTH = (CX_MAX - CX_MIN) / IX_MAX;
-double PIXEL_HEIGHT = (CY_MAX - CY_MIN) / IY_MAX;
+double pixel_width{};
+double pixel_height{};
 
 const std::string FILENAME = "new1.ppm";
 const std::string COMMENT = "# ";
@@ -25,29 +25,17 @@ const int ITERATION_MAX = 200;
 constexpr double ESCAPE_RADIUS = 2;
 constexpr double ER2 = ESCAPE_RADIUS * ESCAPE_RADIUS;
 
-unsigned char *IMAGE_DATA;
+unsigned char *image_data;
 
 constexpr int NUM_THREADS = 4;
-int CHUNK_SIZE{};
+int chunk_size{};
 std::thread threads[NUM_THREADS];
-
-int convert_string_to_int(const std::string& str) {
-  try {
-    return std::stoi(str);
-  } catch (const std::invalid_argument& ex) {
-    std::cerr << "Invalid number: " << str << '\n';
-    exit(1);
-  } catch (const std::out_of_range& ex) {
-    std::cerr << "Number out of range: " << str << '\n';
-    exit(1);
-  }
-}
 
 void computeMandelbrot(int thread_num, int start, int end) {
   for (int iY = start; iY < end; iY++) {
-    for (int iX = 0; iX < IX_MAX; iX++) {
-      double Cx = CX_MIN + iX * PIXEL_WIDTH;
-      double Cy = CY_MIN + iY * PIXEL_HEIGHT;
+    for (int iX = 0; iX < ix_max; iX++) {
+      double Cx = CX_MIN + iX * pixel_width;
+      double Cy = CY_MIN + iY * pixel_height;
 
       double Zx = 0.0;
       double Zy = 0.0;
@@ -64,10 +52,12 @@ void computeMandelbrot(int thread_num, int start, int end) {
         Iteration++;
       }
 
+      int iX3 = iX * 3;
+      int common = iY * ix_max * 3 + iX3;
       if (Iteration == ITERATION_MAX) {
-        IMAGE_DATA[iY * IX_MAX * 3 + iX * 3] = 0;
-        IMAGE_DATA[iY * IX_MAX * 3 + iX * 3 + 1] = 0;
-        IMAGE_DATA[iY * IX_MAX * 3 + iX * 3 + 2] = 0;
+        image_data[common] = 0;
+        image_data[common + 1] = 0;
+        image_data[common + 2] = 0;
       } else {
         // Prevent black color in the first thread
         int base = thread_num + 1;
@@ -77,39 +67,19 @@ void computeMandelbrot(int thread_num, int start, int end) {
         int g = (base * 100) % 256;
         int b = (base * 150) % 256;
 
-        IMAGE_DATA[iY * IX_MAX * 3 + iX * 3] = r;
-        IMAGE_DATA[iY * IX_MAX * 3 + iX * 3 + 1] = g;
-        IMAGE_DATA[iY * IX_MAX * 3 + iX * 3 + 2] = b;
+        image_data[common] = r;
+        image_data[common + 1] = g;
+        image_data[common + 2] = b;
       }
     }
   }
 }
 
-int main(int argc, char** argv) {
-  if (argc < 2) { // TODO: Handle number of threads
-    std::cerr << "Program requires resolution and number of threads to be passed.\n";
-    return 1;
-  }
-
-  //int res = std::stoi(argv[1]);
-
-  int res = convert_string_to_int(argv[1]);
-  IX_MAX = res;
-  IY_MAX = res;
-  CHUNK_SIZE = IY_MAX / NUM_THREADS;
-
-  IMAGE_DATA = new unsigned char[IX_MAX * IY_MAX * 3];
-
-  //int ths = convert_string_to_int(argv[2]);
-  //NUM_THREADS = ths;
-
-  std::cout << "Image " << FILENAME << " (" << IX_MAX << 'x' << IY_MAX << ")\n";
-  //std::cout << "Threads: " << NUM_THREADS << '\n';
-
+void generate_image() {
   auto start = std::chrono::high_resolution_clock::now();
   for (int i = 0; i < NUM_THREADS; i++) {
-    int start = i * CHUNK_SIZE;
-    int end = (i == NUM_THREADS - 1) ? IY_MAX : (i + 1) * CHUNK_SIZE;
+    int start = i * chunk_size;
+    int end = (i == NUM_THREADS - 1) ? iy_max : (i + 1) * chunk_size;
     threads[i] = std::thread(computeMandelbrot, i, start, end);
   }
 
@@ -122,12 +92,36 @@ int main(int argc, char** argv) {
   std::cout << "Time: " << time << "ms\n";
 
   std::ofstream fp(FILENAME, std::ios::binary);
-  fp << "P6\n " << COMMENT << "\n " << IX_MAX << "\n " << IY_MAX << "\n "
+  fp << "P6\n " << COMMENT << "\n " << ix_max << "\n " << iy_max << "\n "
      << 255 << '\n';
-  fp.write((char *)IMAGE_DATA, IX_MAX * IY_MAX * 3);
+  fp.write((char *)image_data, ix_max * iy_max * 3);
   fp.close();
 
-  delete[] IMAGE_DATA;
+  delete[] image_data;
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2) { // TODO: Handle number of threads
+    std::cerr << "Program requires resolution and number of threads to be passed.\n";
+    return 1;
+  }
+
+  int res = std::stoi(argv[1]);
+  ix_max = res;
+  iy_max = res;
+  chunk_size = iy_max / NUM_THREADS;
+
+  pixel_width = (CX_MAX - CX_MIN) / ix_max;
+  pixel_height = (CY_MAX - CY_MIN) / iy_max;
+
+  image_data = new unsigned char[ix_max * iy_max * 3];
+
+  //int ths = convert_string_to_int(argv[2]);
+  //NUM_THREADS = ths;
+
+  std::cout << "Image " << FILENAME << " (" << ix_max << 'x' << iy_max << ")\n";
+
+  generate_image();
 
   return 0;
 }

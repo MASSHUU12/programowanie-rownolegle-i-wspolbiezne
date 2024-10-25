b6586b907a2b5b4cdc0c017fa035ecb989d129fa
MASSHUU12 <gawrysiak.maciej@proton.me>
2024-10-25 14:28:21 +0200

    feat(lab4): use mutex?

--- lab4/client.cpp
+++ lab4/client.cpp
@@ -3,39 +3,43 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <thread>
 
 #include "common.hpp"
 
-void setupServerAddress(sockaddr_in &serv_addr) {
+void setupServerAddress(sockaddr_in &serv_addr)
+{
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
 
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+  {
     handleError("Invalid address/ Address not supported");
   }
 }
 
-void connectToServer(const int client_fd, const sockaddr_in &serv_addr) {
+void connectToServer(const int &client_fd, const sockaddr_in &serv_addr)
+{
   int status =
       connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-  if (status < 0) {
+  if (status < 0)
+  {
     handleError("Connection Failed");
   }
 }
 
-int main(const int argc, char const **argv) {
+int main(const int argc, char const **argv)
+{
   int client_fd = createSocket();
-  struct sockaddr_in serv_addr;
+  sockaddr_in serv_addr;
   setupServerAddress(serv_addr);
   connectToServer(client_fd, serv_addr);
 
-  const std::string hello = "Hello from client";
-  sendData(client_fd, hello);
-  std::cout << "Message sent\n";
+  std::thread listener(listenForMessages, client_fd);
+  std::thread sender(sendMessages, client_fd);
 
-  char buffer[BUFFER_SIZE] = {0};
-  readData(client_fd, buffer);
-  std::cout << buffer << '\n';
+  listener.join();
+  sender.join();
 
   close(client_fd);
   return 0;
--- lab4/common.hpp
+++ lab4/common.hpp
@@ -7,35 +7,86 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
 
 const int PORT = 8080;
 const int BUFFER_SIZE = 1024;
 
-inline void handleError(const std::string &message) {
-  std::cerr << "[ERR]" << message << ": " << strerror(errno) << '\n';
-  exit(1);
+std::mutex mtx;
+std::condition_variable cv;
+std::atomic<bool> dataAvailable{false};
+
+inline void handleError(const std::string &message)
+{
+    std::cerr << "[ERR] " << message << ": " << strerror(errno) << '\n';
+    exit(1);
 }
 
-inline int createSocket() {
-  int client_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (client_fd < 0) {
-    handleError("Socket creation error");
-  }
-  return client_fd;
+inline int createSocket()
+{
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd < 0)
+    {
+        handleError("Socket creation error");
+    }
+    return client_fd;
 }
 
-inline void sendData(const int socket, const std::string &message) {
-  if (send(socket, message.c_str(), message.length(), 0) < 0) {
-    handleError("send failed");
-  }
+inline void sendData(const int socket, const std::string &message)
+{
+    if (send(socket, message.c_str(), message.length(), 0) < 0)
+    {
+        handleError("send failed");
+    }
 }
 
-inline void readData(int socket, char *buffer) {
-  ssize_t valread = read(socket, buffer, BUFFER_SIZE - 1);
-  if (valread < 0) {
-    handleError("read failed");
-  }
-  buffer[valread] = '\0'; // Add null terminator
+inline void readData(int socket, char *buffer)
+{
+    ssize_t valread = read(socket, buffer, BUFFER_SIZE - 1);
+    if (valread < 0)
+    {
+        handleError("read failed");
+    }
+    buffer[valread] = '\0'; // Add null terminator
+}
+
+inline void listenForMessages(const int &client_fd)
+{
+    char buffer[BUFFER_SIZE] = {0};
+    while (true)
+    {
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, []
+                    { return dataAvailable.load(); });
+            dataAvailable = false;
+            readData(client_fd, buffer);
+        }
+        if (std::string(buffer).find_first_not_of('\0') != std::string::npos)
+        {
+            std::cout << "Received message: " << buffer << '\n';
+        }
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+}
+
+inline void sendMessages(const int &client_fd)
+{
+    std::string message;
+    while (true)
+    {
+        std::cout << "Enter message to send: ";
+        std::getline(std::cin, message);
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            sendData(client_fd, message);
+            dataAvailable = true;
+            cv.notify_one();
+        }
+        std::cout << "Message sent\n";
+    }
 }
 
 #endif // COMMON_HPP

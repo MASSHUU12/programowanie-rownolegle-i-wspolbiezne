62497fb80ac2581ad4461da20d920ee7817ce3e2
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-12-10 17:41:26 +0100

    feat(e7): setup a button

--- e7/app/src/main/cpp/native-lib.cpp
+++ e7/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,11 @@
 #include <jni.h>
 #include <string>
+#include <iostream>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_e7_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_e7_MainActivity_calculate(JNIEnv *env, jobject thiz) {
+    std::stringstream ss;
+
+    return env->NewStringUTF("Calculate\n");
 }
--- e7/app/src/main/java/com/example/e7/MainActivity.kt
+++ e7/app/src/main/java/com/example/e7/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.e7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import com.example.e7.databinding.ActivityMainBinding
 
@@ -14,16 +15,13 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
     }
 
-    /**
-     * A native method that is implemented by the 'e7' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+    private external fun calculate(): String
+
+    fun onCalculateClick(view: View) {
+        binding.resultText.text = calculate()
+    }
 
     companion object {
         // Used to load the 'e7' library on application startup.
